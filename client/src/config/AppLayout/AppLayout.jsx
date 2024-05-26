f63c22ce92f57a6ecab49a7e15a43d5dcaca0120
lalaghoury <lala.ghoury@gmail.com>
2024-05-26 23:19:28 +0500

    some

--- client/src/config/AppLayout/AppLayout.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./AppLayout.scss";
-
-const AppLayout = ({ children, className = "" }) => {
-  return <div className={`appLayout ${className}`}>{children}</div>;
-};
-
-export default AppLayout;
